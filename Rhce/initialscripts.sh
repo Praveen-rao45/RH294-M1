9ad26dbf484552d99ef73e105a0ee630c04c56ac
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2023-09-21 15:43:37 +0530

    Update initialscripts.sh

--- Rhce/initialscripts.sh
+++ Rhce/initialscripts.sh
@@ -1,4 +1,8 @@
 wget http://localhost/Rhce/inventory    > /dev/null 2>&1
 wget http://localhost/Rhce/ansible.cfg  > /dev/null 2>&1
+mkdir collections
+ansible-galaxy collection install  http://localhost/Rhce/ansible-posix-1.4.0.tar.gz -p collections
+ansible-galaxy collection install  http://localhost/Rhce/redhat-rhel_system_roles-1.0.0.tar.gz -p collections
+ansible-galaxy collection install  http://localhost/Rhce/community-general-7.3.0.tar.gz -p collections
 wget http://localhost/Rhce/admin.yml       > /dev/null 2>&1
 ansible-playbook admin.yml      > /dev/null 2>&1
