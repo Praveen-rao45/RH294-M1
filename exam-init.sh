a86192dd029b0c842af1459d3291b7096cdea0ff
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2024-07-17 23:09:12 +0530

    Update exam-init.sh

--- exam-init.sh
+++ exam-init.sh
@@ -11,7 +11,7 @@ echo -ne '##############             (50%)\r'
 
 cp -rf Rhce  /var/www/html/Rhce     > /dev/null 2>&1
 systemctl  restart httpd   > /dev/null 2>&1
-wget  localhos/Rhce/init-dns.sh  > /dev/null 2>&1
+wget  localhost/Rhce/init-dns.sh  > /dev/null 2>&1
 sh init-dns.sh  > /dev/null 2>&1
 wget  http://localhost/Rhce/initialscripts.sh    > /dev/null 2>&1
 sh initialscripts.sh > /dev/null 2>&1
